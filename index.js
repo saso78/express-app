9baefb398fc66a4915fa4734e0ada58753fe6d58
Saso Trpovski <sasotrpovski@gmail.com>
2024-08-29 14:00:21 +0200

    change

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 
 app.get('/', (req, res) => {
-  res.send('<h1>Hello FROM THE OTHER SIDE OF THE World!</h1>')
+  res.send('<h1>Hello Hello Hello World!</h1>')
 })
 
 const PORT = 3000
