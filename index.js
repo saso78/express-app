3f67deaae850e7bd428e9453f813e902f891bb52
Saso Trpovski <sasotrpovski@gmail.com>
2024-08-29 13:29:21 +0200

    napravena promena

--- index.js
+++ index.js
@@ -2,10 +2,10 @@ const express = require('express')
 const app = express()
 
 app.get('/', (req, res) => {
-  res.send('<h1>Hello World!</h1>')
+  res.send('<h1>Hello FROM THE OTHER SIDE OF THE World!</h1>')
 })
 
-const PORT = 8080
+const PORT = 3000
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
