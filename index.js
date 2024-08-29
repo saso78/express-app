6d44451f9cbd78b01c88821e46a1c77fe75941ed
Saso Trpovski <sasotrpovski@gmail.com>
2024-08-29 15:38:15 +0200

    try 1

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 
 app.get('/', (req, res) => {
-  res.send('<h1>Hello Sasho, how is your World!</h1>')
+  res.send('<h1>Hello!</h1>')
 })
 
 const PORT = 3000
