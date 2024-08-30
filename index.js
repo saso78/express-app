c773206adc43cbb661f4273476606ce4289f6a1d
Saso Trpovski <sasotrpovski@gmail.com>
2024-08-30 10:22:46 +0200

    izmena da ne se koristi dockerfile za express

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 
 app.get('/', (req, res) => {
-  res.send('<h1>Hello yellow <h1>')
+  res.send('<h1>Hello yellow dog <h1>')
 })
 
 const PORT = 3000
